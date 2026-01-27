b27ffcf6fe17b1f1c95a4a8cf6f7349e1db605b5
shiba <shiba.studio.tw@gmail.com>
2026-01-27 15:58:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hugbear-v5';
+const CACHE_NAME = 'hugbear-v6';
 const ASSETS = [
     './',
     './index.html',
@@ -60,6 +60,7 @@ self.addEventListener('fetch', (event) => {
             })
     );
 });
+
 
 
 
